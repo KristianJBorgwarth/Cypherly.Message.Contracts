561f5faa4f3f0a5f308d103975c07d65a6e58f8b
kristianjborgwarth <10348902@pm.me>
2025-07-13 19:19:33 +0200

    ref: remove public setter for id on emailmsg

--- Cypherly.Message.Contracts/Messages/Email/SendEmailMessage.cs
+++ Cypherly.Message.Contracts/Messages/Email/SendEmailMessage.cs
@@ -5,7 +5,7 @@ namespace Cypherly.Message.Contracts.Messages.Email;
 public sealed class SendEmailMessage : IBaseMessage
 {
     public DateTime OccurredOn { get;} = DateTime.UtcNow;
-    public Guid Id { get; init; } = Guid.NewGuid();
+    public Guid Id { get; } = Guid.NewGuid();
     public required Guid CorrelationId { get; init; }
     public required Guid? CausationId { get; init; }
     public required string To { get; init; }
