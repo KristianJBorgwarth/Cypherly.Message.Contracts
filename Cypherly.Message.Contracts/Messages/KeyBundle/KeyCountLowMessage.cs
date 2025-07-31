fe22e91d6f53df87f94f5b806fe7fd5dc132d4d0
Kristian <10348902@pm.me>
2025-07-31 20:16:13 +0200

    feat: key count low message

---
+++ Cypherly.Message.Contracts/Messages/KeyBundle/KeyCountLowMessage.cs
@@ -0,0 +1,13 @@
+﻿using Cypherly.Message.Contracts.Abstractions;
+
+namespace Cypherly.Message.Contracts.Messages.KeyBundle;
+
+public class KeyCountLowMessage : IBaseMessage
+{
+    public Guid Id { get; }  = Guid.NewGuid();
+    public DateTime OccurredOn { get; }  = DateTime.UtcNow;
+    public required Guid CorrelationId { get; init; }
+    public Guid? CausationId { get; init; }
+    public required Guid TenantId { get; init; }
+    public required Guid ConnectionId { get; init; }
+}
