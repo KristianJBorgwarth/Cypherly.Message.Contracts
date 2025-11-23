e6a9e8d5762a5b112ad07882c228f103069357ad
Kristian <10348902@pm.me>
2025-11-23 18:29:48 +0100

    extend with proxy id repsonse

---
+++ Cypherly.Message.Contracts/Responses/Device/ConnectionIdsProxyResponse.cs
@@ -0,0 +1,16 @@
+using Cypherly.Message.Contracts.Abstractions;
+
+namespace Cypherly.Message.Contracts.Responses.Device
+{
+    public class ConnectionIdsProxyResponse : IBaseMessage
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public DateTime OccurredOn { get; } = DateTime.UtcNow;
+        public required Guid CorrelationId { get; init; }
+        public Guid? CausationId { get; init; } = null;
+        public bool IsSuccess { get; init; } = true;
+        public string? Error { get; init; } = null;
+        public string? UserTag { get; init; } = null;
+        public Dictionary<Guid, List<Guid>>? ConnectionIds { get; init; } = null;
+    }
+}
