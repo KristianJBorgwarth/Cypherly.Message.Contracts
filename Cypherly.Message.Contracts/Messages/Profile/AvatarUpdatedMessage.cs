94f7d611f328a513be47b10659e0b3f635fe8d35
Kristian Borgwarth <10348902@pm.me>
2026-05-25 11:27:38 +0200

    fix avatarkey type

--- Cypherly.Message.Contracts/Messages/Profile/AvatarUpdatedMessage.cs
+++ Cypherly.Message.Contracts/Messages/Profile/AvatarUpdatedMessage.cs
@@ -9,6 +9,6 @@ public sealed class AvatarUpdatedMessage : IBaseMessage
     public required Guid CorrelationId { get; init; }
     public Guid? CausationId { get; init; } = null;
     public required string UserTag { get; init; }
-    public required string AvatarKey { get; init; }
+    public required Guid AvatarKey { get; init; }
     public required IReadOnlyCollection<Guid> ConnectionIds { get; init; }
 }
